e47c4d2af853b16545fb3a4ef3020fe961915065
themassiveone <me@themassiveone.net>
2025-09-10 15:50:11 +0200

    refactor: domain parts from DotChaser into separate project

---
+++ DotChaser/DotChaser/Game.cs
@@ -0,0 +1,47 @@
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace DotChaser
+{
+    public class Game
+    {
+        Dictionary<Vector2, int> grid = new();
+        public IEnumerable<Player> Players => _players.Values;
+        private readonly Dictionary<int, Player> _players = new();
+
+        public Game(int height, int width)
+        {
+            _players.Add(1000, new Player(new Vector2(3, 3), new Vector2(0, 0), 1000));
+            for (int y = height - 1; y >= 0; y--)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (_players.Values.Any(p => p.Direction == new Vector2(x, y)))
+                        grid[new Vector2(x, y)] = 1000;
+                    else
+                        grid[new Vector2(x, y)] = 0;
+                }
+            }
+        }
+
+
+        public void Simulate(ConcurrentQueue<Vector2> inputChanges)
+        {
+            foreach (var inputChange in inputChanges)
+            {
+                var player = _players[1000];
+                player.ChangeDirection(inputChange);
+            }
+
+            foreach (var player in _players.Values)
+            {
+                var newPosition = player.Position + player.Direction;
+                grid[player.Position] = 0;
+                grid[newPosition] = player.ID;
+                player.UpdatePosition(newPosition);
+            }
+        }
+    }
+}
---
+++ DotChaser/DotChaser/Player.cs
@@ -0,0 +1,28 @@
+using System.Numerics;
+
+namespace DotChaser
+{
+    public class Player
+    {
+        public Player(Vector2 position, Vector2 direction, int id)
+        {
+            Position = position;
+            Direction = direction;
+            ID = id;
+        }
+
+        public Vector2 Direction { get; private set; }
+        public Vector2 Position { get; private set; }
+        public int ID { get; }
+
+        public void ChangeDirection(Vector2 inputChange)
+        {
+            Direction = inputChange;
+        }
+
+        public void UpdatePosition(Vector2 newPosition)
+        {
+            Position = newPosition;
+        }
+    }
+}
