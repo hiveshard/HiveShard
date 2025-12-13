c40238b04e8bc74ced6cdf7cb7078e27c0e3d58f
themassiveone <business.daniel.mass@gmail.com>
2025-12-13 15:34:23 +0100

    ci: remove version requirement from verify step

--- release.sh
+++ release.sh
@@ -3,4 +3,6 @@ FOLDER=$1
 PACKAGE=$FOLDER/$2/$2.csproj
 VERSION=$3
 
+dotnet build $PACKAGE -c Release -p:Version=$VERSION
+dotnet pack $PACKAGE -c Release -p:Version=$VERSION -o ./nupkg
 dotnet nuget push ./nupkg/$2.$VERSION.nupkg --api-key $NUGET_API_KEY --source https://api.nuget.org/v3/index.json
--- verify.sh
+++ verify.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 FOLDER=$1
 PACKAGE=$FOLDER/$2/$2.csproj
-VERSION=$3
 
-dotnet build $PACKAGE -c Release -p:Version=$VERSION
-dotnet pack $PACKAGE -c Release -p:Version=$VERSION -o ./nupkg
+dotnet build $PACKAGE -c Release -p:Version=0.0.1-demo
+dotnet pack $PACKAGE -c Release -p:Version=0.0.1-demo -o ./nupkg-demos
