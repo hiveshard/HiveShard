5be5abbd83ae557b37dda8c8e26168011767ce2f
themassiveone <business.daniel.mass@gmail.com>
2026-01-02 11:21:27 +0100

    feat: ticker 3/14

--- HiveShard.Ticker/DistributedTicker.cs
+++ HiveShard.Ticker/DistributedTicker.cs
@@ -33,24 +33,46 @@ public class DistributedTicker
 
     private ConcurrentDictionary<long, ConcurrentHashSet<EmitterIdentity>> _completedEventTicks = new();
 
-    private long _currentTick;
+    private bool _currentRoundOngoing = false;
+    
+    // initialized without tick
+    private long _currentTick = -1;
+    
+    // no tick allowed at first
+    private long _allowanceTick = -1;
     private void HandleGlobalTicks(Consumption<Tick> consumption)
     {
         var messageTick = consumption.Message.TickNumber;
         if(messageTick < _currentTick)
             return;
 
-        throw new NotImplementedException();
+        if (messageTick > _allowanceTick)
+            _allowanceTick = messageTick;
+
+        
+        if(messageTick == _currentTick + 1 && !_currentRoundOngoing)
+        {
+            _currentTick += 1;
+            var eventPartition = new Partition(_eventRepository.GetEventOrder(_config.EventType));
+            _simpleFabric.Send("ticks", eventPartition,
+                new Tick(_currentTick, [], DateTime.Now, _config.EventType.FullName!));
+        }
+            
     }
 
     private void HandleEventSpecificCompletedTick(Consumption<CompletedTick> consumption)
     {
+        if (consumption.Message.Tick > _currentTick)
+            throw new NotImplementedException("We did not start this round of ticks, no ticker redundancy yet");
+        
         var hashSet = _completedEventTicks.GetOrAdd(consumption.Message.Tick, 
             _ => new ConcurrentHashSet<EmitterIdentity>());
         
         // skip duplicates
         if (!hashSet.Contains(consumption.Message.EmitterIdentity))
             return;
+        
+        hashSet.Add(consumption.Message.EmitterIdentity);
 
         foreach (var eventEmitterType in _eventRepository.GetEmitters(_config.EventType.FullName!))
         {
@@ -59,11 +81,15 @@ public class DistributedTicker
                 return;
         }
         
-        // finalize event scope tick
-        _currentTick += 1;
+        FinalizeTick();
+    }
+
+    private void FinalizeTick()
+    {
         var thisTickersPartition = new Partition(_eventRepository.GetEventOrder(_config.EventType));
         _simpleFabric.Send("completed-ticks", thisTickersPartition,
             new CompletedTick(_config.EmitterType.Identity, _currentTick, _config.EventType.FullName!, []));
     }
-    
+
+
 }
